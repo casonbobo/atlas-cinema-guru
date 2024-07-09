015a76c10060f8fe591047a2ed53cf826c7e4bd6
casonbobo <casonsbobo@gmail.com>
2024-07-08 22:21:10 -0500

    This is the rest of auth. functional or not the code is there

--- atlas-cinema-guru-app/src/routes/auth/Authentication.js
+++ atlas-cinema-guru-app/src/routes/auth/Authentication.js
@@ -1,23 +1,53 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './auth.css';
 
-const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
-  const [_switch, setSwitch] = useState(true);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleSignIn = () => {
-    setIsLoggedIn(true);
-    setUserUsername(username);
-  };
+  const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [_switch, setSwitch] = useState(true);
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+  
+    const handleSignIn = () => {
+      setIsLoggedIn(true);
+      setUserUsername(username);
+    };
+  
+    const handleSignUp = () => {
+      setIsLoggedIn(false);
+      setUserUsername('');
+    };
 
-  const handleSignUp = () => {
-    setIsLoggedIn(false);
-    setUserUsername('');
-  };
+      try {
+        if (_switch) {
+          const response = await axios.post('/api/auth/login', {
+            username,
+            password,
+          });
+  
+          localStorage.setItem('accessToken', response.data.token);
+          setUserUsername(username);
+          setIsLoggedIn(true);
+        } else {
+          const response = await axios.post('/api/auth/register', {
+            username,
+            password,
+          });
+  
+          localStorage.setItem('accessToken', response.data.token);
+          setUserUsername(username);
+          setIsLoggedIn(true);
+        }
+      } catch (error) {
+        console.error('Error:', error.message);
+      }
+    };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Username"
