ad3cf99e07a44e5aa8df49623d564876eebe75bb
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-07-14 23:17:32 -0500

    Changed movie cards and the fav and wl system

--- atlas-cinema-guru-app/src/components/movies/MovieCard.js
+++ atlas-cinema-guru-app/src/components/movies/MovieCard.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStarSolid, faClock as faClockSolid } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStarRegular, faClock as faClockRegular } from '@fortawesome/free-regular-svg-icons';
 import axios from 'axios';
 import './movies.css';
 
@@ -17,7 +18,6 @@ const MovieCard = ({ movie }) => {
         console.error(error);
       }
     };
-
     const fetchWatchLaterStatus = async () => {
       try {
         const response = await axios.get(`/api/titles/watchlater/${movie.imdbId}`);
@@ -26,43 +26,47 @@ const MovieCard = ({ movie }) => {
         console.error(error);
       }
     };
-
     fetchFavoriteStatus();
     fetchWatchLaterStatus();
   }, [movie.imdbId]);
 
   const handleClick = async (type) => {
+    const endpoint = `http://localhost:8000/api/titles/${type}/${movie.imdbID}`;
     try {
+      if ((type === 'favorite' && !isFavorite) || (type === 'watchlater' && !isWatchLater)) {
+        await axios.post(endpoint, {}, header);
+      } else {
+        await axios.delete(endpoint, header);
+      }
+
       if (type === 'favorite') {
-        if (isFavorite) {
-          await axios.delete(`/api/titles/favorite/${movie.imdbId}`);
-          setIsFavorite(false);
-        } else {
-          await axios.post(`/api/titles/favorite/${movie.imdbId}`);
-          setIsFavorite(true);
-        }
+        setIsFavorite(!isFavorite);
       } else if (type === 'watchlater') {
-        if (isWatchLater) {
-          await axios.delete(`/api/titles/watchlater/${movie.imdbId}`);
-          setIsWatchLater(false);
-        } else {
-          await axios.post(`/api/titles/watchlater/${movie.imdbId}`);
-          setIsWatchLater(true);
-        }
+        setIsWatchLater(!isWatchLater);
       }
     } catch (error) {
-      console.error(error);
+      console.error(`Error updating ${type} status:`, error);
     }
   };
 
   return (
     <li className='movie-card-li'>
-      {isFavortie ?
-        <FontAwesomeIcon icon={faStarSolid} className='card-icon' onClick={() => handleClick('favorite')} />
-        :
-        <FontAwesomeIcon icon={faStarRegular} className='card-icon' onClick={() => handleClick('favorite')} /> 
-        }
-
+      <div>
+          <FontAwesomeIcon icon={isFavorite ? faStar : faStarRegular} onClick={() => handleClick('favorite')} />
+          <FontAwesomeIcon icon={isWatchLater ? faClockRegular : faClock} onClick={() => handleClick('watchlater')} />
+      </div>
+      <div className = "movie-card-info">
+        <img src={imageLoaded ? movie.imageurls : placeHolderImage} alt={movie.title} onError={handleImageError} />
+        <div className="movie-info">
+          <h3>{movie.title}</h3>
+          <p>{truncateText(movie.synopsis, 100)}</p>
+          <ul className="tag">
+            {movie.genres.map((genre) => (
+              <Tag key={genre} genre={genre}/>
+            ))}
+          </ul>
+        </div>
+      </div>
     </li>
   );
 };
